c94132567b4818db2594080b4c8c80088b9f6eb1
Ramprits <21260321+Ramprits@users.noreply.github.com>
2023-06-03 12:39:31 +0530

    2 files modified, 9 deleted

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,17 @@
 import { Stack } from '@mui/material';
+import { Link } from 'react-router-dom';
 
 export const Navbar = () => {
   return (
     <Stack>
-      <img src="" alt="" />
+      <Link to="/">
+        <img
+          src="https://res.cloudinary.com/rps-technologies/image/upload/v1685775974/gym-application/Logo.png"
+          alt=""
+          style={{ height: '48px', width: '48px', margin: '0 20px' }}
+        />
+      </Link>
+      <Stack></Stack>
     </Stack>
   );
 };
